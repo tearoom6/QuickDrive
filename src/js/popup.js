fdb96bcb08a66a724adc2a7f27f7d4bd5a7e5ed1
Tomohiro Murota <tomohiro.murota@gmail.com>
2015-02-13 02:15:48 +0900

    全文検索機能追加

--- src/js/popup.js
+++ src/js/popup.js
@@ -8,19 +8,26 @@ module.controller('PopupCtrl', ['$scope', '$filter', '$interval', function Popup
   const MAX_LIST_COUNT = 20;
   const TYPE_RECENT = 'recent';
   const TYPE_FAVORITE = 'favorite';
+  const TYPE_SEARCH = 'search';
   $scope.TYPE_RECENT = TYPE_RECENT;
   $scope.TYPE_FAVORITE = TYPE_FAVORITE;
+  $scope.TYPE_SEARCH = TYPE_SEARCH;
   // angular functions
   var orderBy = $filter('orderBy');
   // resources
   $scope.title = chrome.i18n.getMessage('extName');
   $scope.reset = chrome.i18n.getMessage('reset');
+  $scope.search = chrome.i18n.getMessage('search');
+  $scope.clear = chrome.i18n.getMessage('clear');
+  $scope.search_text = chrome.i18n.getMessage('search_text');
+  $scope.search_result = chrome.i18n.getMessage('search_result');
   $scope.recent = chrome.i18n.getMessage('recent');
   $scope.favorite = chrome.i18n.getMessage('favorite');
   $scope.lastViewedAt = chrome.i18n.getMessage('lastViewedAt');
   $scope.dateFormat = chrome.i18n.getMessage('dateFormat');
   // default value
   $scope.active = TYPE_FAVORITE;
+  $scope.searchText = '';
   $scope.items = [];
   
   /* 定期処理 */
@@ -41,6 +48,21 @@ module.controller('PopupCtrl', ['$scope', '$filter', '$interval', function Popup
   /* タブの状態 */
   $scope.isActiveTab = function(type) {
     return $scope.active == type;
+  };
+  
+  /* 検索BOX */
+  $scope.searchItems = function() {
+  	if ($scope.searchText == null || $scope.searchText == '') {
+  	  return;
+  	}
+  	$scope.active = TYPE_SEARCH;
+    listLimitedFiles({'q': 'trashed = false and fullText contains \'' + $scope.searchText + '\''}, MAX_LIST_COUNT, function(result) {
+      $scope.items = orderBy(result, 'lastViewedByMeDate', true);
+      $scope.$apply(); // 強制画面更新
+    });
+  };
+  $scope.clearSearchBox = function() {
+    $scope.searchText = '';
   };
   
   /* 認証トークンのリセット */
@@ -68,6 +90,9 @@ module.controller('PopupCtrl', ['$scope', '$filter', '$interval', function Popup
 //           $scope.items = orderBy(result, 'modifiedByMeDate', true);
           $scope.$apply(); // 強制画面更新
         });
+      } else if (type == TYPE_SEARCH) {
+        $scope.active = TYPE_SEARCH;
+        $scope.items = [];
       }
     });
   };
